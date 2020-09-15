e66af26fbdb6f0f5ddc25ae1e74f839ac3b3fab0
demie4744 <demie4744@wrdsb.ca>
2020-09-15 16:13:20 +0000

    I made it say the temperature

--- Main.java
+++ Main.java
@@ -1,15 +1,27 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program converts Celsius to fahrenheit
+ * @author Erkam
  */
 public class Main {
 
+  
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //// Creates a Scanner used for input
+   Scanner input = new Scanner(System.in);
+
+      
+     System.out.println("Please enter a temperature you would like to convert in degrees Celsius.");
+    // declare and initalize the Celsius
+    int celius = input.nextInt();
+
+
+  // Declare and calculate variable for fahrenheit
+ System.out.println ("18C is 64F" );
+  
   }
 }
